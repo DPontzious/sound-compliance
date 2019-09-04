fb41abed042ad2e2d778f8b1663b1737286829f0
Danelle Pontzious <danellepontzious@Danelles-Air.domain>
2019-09-04 16:54:06 -0700

    first commit

---
+++ src/utils/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "./pages/Main/Main";
+import Trip from "./pages/Trip/Trip";
+import NoMatch from "./pages/NoMatch/Nomatch";
+import Nav from "./components/Nav/index"
+import Events from "./pages/Events/index"
+import Jumbo from "./components/Jumbo/index"
+import Register from "./pages/Register/Register"
+import SignExpanded from "./components/Login/SignExpanded"
+import SavedTrips from "./pages/SavedTrips/SavedTrips"
+
+
+
+
+function App() {
+    return (
+        <Router>
+            <Jumbo />
+            <Nav />
+            <Switch>
+                <Route exact path="/" component={Main} />
+                <Route exact path="/events" component={Events} />
+                <Route exact path="/trip" component={Trip} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/savedTrips" component={SavedTrips} />
+                <Route exact path="/" component={SignExpanded} />
+                <Route component={NoMatch} />
+            </Switch>
+
+        </Router >
+    );
+}
+
+export default App;
